265598d8b8835e30d8a7087a346d511910ec904c
kishore kumar <kishore.behera2010@gmail.com>
2026-05-02 10:48:00 +0530

    Fix SPA catch-all route for Express 5

--- server/static.ts
+++ server/static.ts
@@ -12,8 +12,8 @@ export function serveStatic(app: Express) {
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
-  app.use("/{*path}", (_req, res) => {
+  // fall through to index.html for all unmatched routes (SPA routing)
+  app.use((_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
